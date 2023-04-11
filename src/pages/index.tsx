017a20c12118a7a6da7bba6eb0d7402bfaba3e3e
Deren Somsanith <derensomsanith@gmail.com>
2023-04-11 18:36:21 -0500

    very lame

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { PhotoGrid } from "~/components/PhotoGrid";
+
+import { PhotoGrid } from "../components/PhotoGrid";
 
 const Home: NextPage = () => {
   return (
